3e0d79d7a1928200d91a40b245b6b23f5917abe4
johnbnlg <johnbnlg@gmail.com>
2017-05-20 09:47:32 +0000

    Update sys.sh

--- sys.sh
+++ sys.sh
@@ -1,34 +1,33 @@
 #!/bin/sh
 
 # Concepteur: FOTSO KUATE Franck arnaud; Matricule: 16b884fs
+
 usage(){
 	echo "Bienvenu sur mon script sys.\n"
 	echo "Choisir l'option à executer."
-	echo "a) Informations des utilisateur enregistrés il y a trois jours."
+	echo "a) Informations des utilisateurs enregistrés il y a trois jours."
 	echo "b) Acquisition, installation et lancement de l'environement XAMPP."
 	echo "c) Archivage des éléments du répertoire personel qui ont été modifiés par l'utilisateur sudoer il y a deux jours dans un périphérique externe."
 	echo "d) Informations sur l'utilisation du disque, de la mémoire, du processeur et de la swap."
-	echo "q/Q) Quitter."
+	echo "q/Q) Quitter.\n"
 }
 
 usersInfo(){
-	echo "loginname:password:lcdate:mindays:maxdays:ndays1:ndays2:expiration:reserved"
-	awk -v date=$(date +%s) -F ":" '$2>=date-3*24*3600 && $2<=date-2*24*3600 {print $0}' /etc/shadow
+	for homedir in `find /opt/* -maxdepth 0 -type d -mtime -3`
+	do
+		awk -v user=${homedir##*/} -F ":" 'user==$1{print $0}' /etc/passwd
+	done
 }
 
 xamppSetup(){
 	wget https://downloadsapachefriends.global.ssl.fastly.net/xampp-files/7.1.4/xampp-linux-x64-7.1.4-0-installer.run?from_af=true
 	sudo chmod +x xampp-linux-x64-7.1.4-0-installer.run
 	sudo ./xampp-linux-x64-7.1.4-0-installer.run
-	/bin/xampp_control
+	sudo /opt/lampp/lampp start
 }
 
 compression(){
-	
-	for file in `find /home/sudoer -type f -mtime -3`
-	do
-		tar -cf /dev/sdb1/$file.tar $file
-	done
+	find /home/sudoer/* -maxdepth 0 -type fd -mtime -2 -exec tar -rvf /dev/sdb1/suvegarde.tar {} \
 }
 
 sysInfo(){
@@ -36,10 +35,10 @@ sysInfo(){
 	df -h
 	
 	echo "Utilisation de la mémoire et de la swap"
-	free -m
+	free -h
 	
-	echo "Informations sur le processeur"
-	lscpu
+	echo "Utilisation du processeur"
+	top
 }
 
 usage
